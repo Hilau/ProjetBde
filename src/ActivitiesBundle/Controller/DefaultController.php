7d439079297b8424cbad457689e9011c5301e6c1
Hilau <hugo.huilier@viacesi.fr>
2017-04-12 22:38:27 +0200

    Commit initial

---
+++ src/ActivitiesBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ActivitiesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('ActivitiesBundle:Default:index.html.twig');
+    }
+}
