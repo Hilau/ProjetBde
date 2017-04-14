bdf584b048f2206fb262754b685ea5a21964a724
Hilau <hugo.huilier@viacesi.fr>
2017-04-14 12:06:54 +0200

    Installation et configuration du FOSUserBundle

--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -5,10 +5,32 @@ namespace UserBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping\AttributeOverrides;
+use Doctrine\ORM\Mapping\AttributeOverride;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="users")
+ *
+ * @AttributeOverrides({
+ *     @AttributeOverride(name="username",
+ *         column=@ORM\Column(
+ *             name="username",
+ *             type="string",
+ *             length=255,
+ *             nullable=true
+ *         )
+ *     ),
+ *     @AttributeOverride(name="usernameCanonical",
+ *         column=@ORM\Column(
+ *             name="username_canonical",
+ *             type="string",
+ *             length=255,
+ *             nullable=true
+ *         )
+ *     ),  
+ * })
  */
 class User extends BaseUser
 {
@@ -19,9 +41,159 @@ class User extends BaseUser
      */
     protected $id;
 
+     /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "Votre nom doit contenir {{ limit }} caractères minimum",
+     *      maxMessage = "Votre nom doit contenir {{ limit }} caractères maximum"
+     * )
+     */
+    protected $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Type("string")
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 50,
+     *      minMessage = "Votre prénom doit contenir {{ limit }} caractères minimum",
+     *      maxMessage = "Votre prénom doit contenir {{ limit }} caractères maximum"
+     * )
+     */
+    protected $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $avatar;
+
+    /**
+     * @ORM\Column(type="integer", length=11)
+     * @Assert\NotBlank()
+     * @Assert\Type("integer")
+     * @Assert\Range(
+     *      min = 1,
+     *      max = 5,
+     *      minMessage = "Votre promotion doit être supérieur ou égale à {{ limit }}",
+     *      maxMessage = "Votre promotion doit être inférieur ou égale à {{ limit }}"
+     * )
+     */
+    protected $promotion;
+
+
     public function __construct()
     {
         parent::__construct();
         
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string 
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string 
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     * @return User
+     */
+    public function setAvatar($avatar)
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string 
+     */
+    public function getAvatar()
+    {
+        return $this->avatar;
+    }
+
+    /**
+     * Set promotion
+     *
+     * @param integer $promotion
+     * @return User
+     */
+    public function setPromotion($promotion)
+    {
+        $this->promotion = $promotion;
+
+        return $this;
+    }
+
+    /**
+     * Get promotion
+     *
+     * @return integer 
+     */
+    public function getPromotion()
+    {
+        return $this->promotion;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $username = $this->getNom()." ".$this->getPrenom();
+        parent::setEmail($email);
+        $this->setUsername($username);
     }
 }
