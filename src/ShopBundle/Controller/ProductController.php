b343801f10c8df60bdbd647058f9e85ea5b54b6c
Mélanie Gstalter <melanie.gstalter@viacesi.fr>
2017-04-18 16:24:06 +0200

    Modification de toute la boutique et gestion des articles de la boutique

--- src/ShopBundle/Controller/ProductController.php
+++ src/ShopBundle/Controller/ProductController.php
@@ -7,7 +7,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use ShopBundle\Entity\Basket;
+use ShopBundle\Entity\Product;
 use ShopBundle\Entity\Category;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class ProductController extends Controller 
 {
@@ -55,11 +62,36 @@ class ProductController extends Controller
 			];
 		}
 
+		$dateActuelle = new \DateTime("now");
+		$interval = clone($dateActuelle);
+		$interval->sub(new \DateInterval('P7D'));
+		$nouveaute = [];
+		$articlePopulaire = [];
+
+		$chaineDescription = [];
+
+		$query = $productRepository->createQueryBuilder('a')                            
+                            ->orderBy('a.nbAchat', 'DESC')
+                            ->getQuery();
+
+        $bestOfProduct = $query->setMaxResults(2)->getResult();
+
+		foreach ($listArticle as $article) {
+			$dateArticle = $article->getDate();
+			if($dateArticle >= $interval && $dateArticle <= $dateActuelle)
+			{
+				$nouveaute[] = $article;
+			}
+			$chaineDescription[$article->getId()] = substr($article->getDescription(), 0, 35);
+		}
+
 		return $this->render('ShopBundle::shopAcceuil.html.twig', array(
-			'listArticle' => $listArticle,
+			'nouveaute' => $nouveaute,
 			'productRepository' => $productRepository,
 			'productsInfo' => $productsInfo,
-			'categories' => $categories
+			'categories' => $categories,
+			'bestOfProduct' => $bestOfProduct,
+			'chaineDescription' => $chaineDescription
 			));
 	}
 
@@ -74,6 +106,131 @@ class ProductController extends Controller
 			'listArticle' => $listArticle,
 			'categories' => $categories
 			));
+	}
+
+	/**
+	 * @Route("editShop", name="editShop")
+	 */
+	public function editShopAction(){
+		$products= $this->getDoctrine()->getManager()->getRepository('ShopBundle:Product')->findAll();
+		$categories= $this->getDoctrine()->getManager()->getRepository('ShopBundle:Category')->findAll();
+
+		return $this->render('ShopBundle::editShop.html.twig', array(
+			'products' => $products,
+			'categories' => $categories
+			));
+	}
+
+	/**
+	 * @Route("addProduct", name="addProduct")
+	 */
+	public function addProductAction(Request $request){
+		$products= $this->getDoctrine()->getManager()->getRepository('ShopBundle:Product')->findAll();
+		$categories= $this->getDoctrine()->getManager()->getRepository('ShopBundle:Category')->findAll();
+		$em = $this->getDoctrine()->getManager();
+		$product = new Product();
+
+		$form = $this->createFormBuilder($product)
+	        ->add('name', TextType::class)
+	        ->add('description', TextareaType::class)
+	        ->add('price', MoneyType::class)
+	        ->add('category', EntityType::class, array(
+	        	'class' => 'ShopBundle:Category',
+	        	'choice_label' => 'name',
+	        	'multiple' => false,
+	        	'expanded' => false
+	        	))
+	        ->add('image', TextType::class)
+	        ->getForm();
+	        
+		$form->handleRequest($request);
+
+	    if ($form->isSubmitted() && $form->isValid()) {
+	       	$product = $form->getData();
+
+	       	$category = $product->getCategory();
+	       	$idCat = $this->getDoctrine()->getManager()->getRepository('ShopBundle:Category')->findOneByName($category->getName());
+	       	
+	       	$product->setDate(new \DateTime('now'));
+	       	$product->setNbAchat(0);
+
+	       	$product->setCategory($idCat);
+	        $em->persist($product);
+	        $em->flush();
+
+	        $this->addFlash('success', 'Votre produit a été ajouté');
+	    } else if($form->isSubmitted() && !$form->isValid()) {
+	    	$this->addFlash('error', 'Votre produit n\'est pas valide');
+	    }
+
+	
+		return $this->render('ShopBundle::addProduct.html.twig', array(
+			'products' => $products,
+			'categories' => $categories,
+			'form'=> $form->createView()
+			));
+	}
+
+	/**
+	 * @Route("updateProduct/{id}", name="updateProduct")
+	 */
+	public function updateProductAction(Request $request, $id){
+		$em = $this->getDoctrine()->getManager();
+		$product = $this->getDoctrine()->getManager()->getRepository('ShopBundle:Product')->findOneById($id);
+
+		$form = $this->createFormBuilder($product)
+	        ->add('name', TextType::class)
+	        ->add('description', TextareaType::class)
+	        ->add('price', MoneyType::class)
+	        ->add('category', EntityType::class, array(
+	        	'class' => 'ShopBundle:Category',
+	        	'choice_label' => 'name',
+	        	'multiple' => false,
+	        	'expanded' => false
+	        	))
+	        ->add('image', TextType::class)
+	        ->getForm();
+	        
+		$form->handleRequest($request);
+
+	    if ($form->isSubmitted() && $form->isValid()) {
+	       	$product = $form->getData();
+
+	       	$category = $product->getCategory();
+	       	$idCat = $this->getDoctrine()->getManager()->getRepository('ShopBundle:Category')->findOneByName($category->getName());
+	       	
+	       	$product->setDate(new \DateTime('now'));
+	       	$product->setNbAchat($product->getNbAchat());
+
+	       	$product->setCategory($idCat);
+	        $em->flush();
+
+	        $this->addFlash('success', 'Votre produit a été modifié');
+	    } else if($form->isSubmitted() && !$form->isValid()) {
+	    	$this->addFlash('error', 'Votre produit n\'est pas valide');
+	    }
+
+	
+		return $this->render('ShopBundle::updateProduct.html.twig', array(
+			'product' => $product,
+			'form'=> $form->createView()
+			));
+	}
+
+	/**
+	* @Route("deleteProduct/{id}", name="deleteProduct")
+	*/
+	public function deletePhotoAction(Request $request, $id){
+		if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') || !$this->container->get('security.authorization_checker')->isGranted('ROLE_TUTEUR')) {
+            return new Response("Erreur");
+        }
+		
+		$suppr = $this->getDoctrine()->getManager()->getRepository('ShopBundle:Product')->findOneById($id);
+		$this->getDoctrine()->getManager()->remove($suppr);
+
+		$this->getDoctrine()->getManager()->flush();
+
+		return $this->redirectToRoute('editShop');
 	}
 
 
